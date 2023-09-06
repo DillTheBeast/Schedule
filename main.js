d3502c78ee3b915b1a9bd25c7cc54a86b01d927f
DillTheBeast <dillonmaltese@gmail.com>
2023-09-06 08:47:55 -0400

    Update main.js

---
+++ main.js
@@ -0,0 +1,18 @@
+const times = {
+    weekday: [0, 800, 805, 915, 1020, 1050, 1200, 1250, 1415, 1520],
+};
+
+const schedule = {
+    NAVY: {
+        dayA: ['violet', 'pink', 'red', 'yellow', 'orange'],
+        dayB: ['green', 'blue', 'tan', 'violet', 'pink'],
+        dayC: ['yellow', 'red', 'orange', 'green', 'blue'],
+        dayD: ['tan', 'violet', 'pink', 'red', 'yellow'],
+        dayE: ['orange', 'green', 'blue', 'tan', 'violet'],
+        dayF: ['pink', 'red', 'yellow', 'orange', 'green'],
+        dayG: ['blue', 'tan', 'violet', 'pink', 'red'],
+        dayH: ['yellow', 'orange', 'green', 'blue', 'tan']
+    }
+};
+
+
