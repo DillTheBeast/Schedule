8ccf8df2d4e9b863070bfb56819bc635adfe715e
DillTheBeast <dillonmaltese@gmail.com>
2023-09-07 08:10:41 -0400

    asd

--- background.js
+++ background.js
@@ -33,46 +33,91 @@ const schedule = {
     }
 };
 
+// function getToday() {
+//     return new Promise((resolve, reject) => {
+//         //Takes date through local computer time
+//         //If date on computer is wrong then date taken is wrong
+//         currentDate = new Date().toISOString().split('T')[0];
+//         //Takes the url for the info
+//         const url = 'https://docs.google.com/spreadsheets/d/1diCphG3V5YP3LJPLDY85_GgixC_jiBHczaUgGC4suF8/edit?usp=sharing';
+//         //Creates new url for me
+//         const cacheBuster = new Date().getTime();
+//         const urlWithCacheBuster = `${url}&_=${cacheBuster}`;
+//         //Making actual link
+//         fetch(urlWithCacheBuster, { cache: "no-store" })    
+//             .then(response => response.text())
+//             .then(data => {
+//         // Splitting the CSV data into lines and parsing
+//         const lines = data.split('\n');
+//         for (let i = 1; i < lines.length; i++) {
+//             const [date, scheduleDay, week] = lines[i].split(',');
+//                 if (date === currentDate) {
+//                     correctDay = scheduleDay.trim()
+//                     console.log(correctDay)
+//                     colors = schedule.NAVY[correctDay];
+//                     let dayIcon = correctDay.slice(-1);
+//                     setIcon(`images/${week}/${dayIcon}-16.png`);
+//                     resolve(colors); // resolve the promise with colors
+//                     showAll();
+//                     return;
+//                 }
+//             }
+//             console.log(`No schedule found for ${currentDate}`);
+//             setIcon(`images/icon16.png`);
+//             hideAll();
+//         })
+//         .catch(error => console.error('An error occurred:', error));
+//     });
+// }
+
+// // ... (The times, schedule, and getToday() function code goes here) ...
+
+// // A function to run when the extension is clicked, or based on other events.
+// function display() {
+//     getToday().then(colors => {
+//         console.log(colors.join(', '));
+//         // You can also send this data to your content script if needed.
+//     });
+// }
+
+// // You can call display() here or based on other events.
+// display();
 function getToday() {
     return new Promise((resolve, reject) => {
-        //Takes date through local computer time
-        //If date on computer is wrong then date taken is wrong
         currentDate = new Date().toISOString().split('T')[0];
-        //Takes the url for the info
-        const url = 'https://docs.google.com/spreadsheets/d/1diCphG3V5YP3LJPLDY85_GgixC_jiBHczaUgGC4suF8/edit?usp=sharing';
-        //Creates new url for me
+        const url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vS3-6MgEPFUcHbLfa7q97_I6BI8CJvLZA0FDPxMwKOEFKYZs1GAw_4CRt6oOIWhMEITpOKzYrW2u7Ef/pub?gid=0&single=true&output=csv';
         const cacheBuster = new Date().getTime();
         const urlWithCacheBuster = `${url}&_=${cacheBuster}`;
-        //Making actual link
         fetch(urlWithCacheBuster, { cache: "no-store" })    
             .then(response => response.text())
             .then(data => {
-        // Splitting the CSV data into lines and parsing
-        const lines = data.split('\n');
-        for (let i = 1; i < lines.length; i++) {
-            const [date, scheduleDay, week] = lines[i].split(',');
-                if (date === currentDate) {
-                    correctDay = scheduleDay.trim()
-                    console.log(correctDay)
-                    colors = schedule.NAVY[correctDay];
-                    let dayIcon = correctDay.slice(-1);
-                    setIcon(`images/${week}/${dayIcon}-16.png`);
-                    resolve(colors); // resolve the promise with colors
-                    showAll();
-                    return;
+                const lines = data.split('\n');
+                for (let i = 1; i < lines.length; i++) {
+                    const [date, scheduleDay, week] = lines[i].split(',');
+                    if (date === currentDate) {
+                        const correctDay = scheduleDay.trim();
+                        const colors = schedule.NAVY[correctDay];
+                        resolve(colors);
+                        return;
+                    }
                 }
-            }
-            console.log(`No schedule found for ${currentDate}`);
-            setIcon(`images/icon16.png`);
-            hideAll();
-        })
-        .catch(error => console.error('An error occurred:', error));
+                console.log(`No schedule found for ${currentDate}`);
+                reject(new Error(`No schedule found for ${currentDate}`));
+            })
+            .catch(error => {
+                console.error('An error occurred:', error);
+                reject(error);
+            });
     });
 }
 
-function display() {
-    if(getToday()) {
-
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.action === "getTodaySchedule") {
+        getToday().then(colors => {
+            sendResponse({ status: "success", colors: colors });
+        }).catch(error => {
+            sendResponse({ status: "error", error: error.toString() });
+        });
+        return true;  // Indicates you wish to send a response asynchronously.
     }
-}
-
+});
--- popup.js
+++ popup.js
@@ -1,8 +1,18 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Interact with background script to get today's schedule
-    chrome.runtime.getBackgroundPage(function(backgroundPage) {
-        backgroundPage.getToday().then(colors => {
-            document.getElementById("schedule").textContent = colors.join(', ');
-        });
-    });
+// document.addEventListener("DOMContentLoaded", function() {
+//     // Interact with background script to get today's schedule
+//     chrome.runtime.getBackgroundPage(function(backgroundPage) {
+//         backgroundPage.getToday().then(colors => {
+//             document.getElementById("schedule").textContent = colors.join(', ');
+//         });
+//     });
+// });
+
+chrome.runtime.sendMessage({ action: "getTodaySchedule" }, response => {
+    const scheduleDiv = document.getElementById("schedule");
+    
+    if (response.status === "success") {
+        scheduleDiv.textContent = response.colors.join(', ');
+    } else {
+        scheduleDiv.textContent = "Error fetching schedule: " + response.error;
+    }
 });
