65a8200c7bb0b69538654b385cc4e8fae46beb7c
DillTheBeast <dillonmaltese@gmail.com>
2023-09-14 20:00:59 -0400

    Update background.js

--- background.js
+++ background.js
@@ -4,19 +4,6 @@ const schedule = {
     NAVY: {
         //Navy + Gold = same
         //No M1 + M2 on schedule
-        //Switch my classes with whatever classes you have
-        dayA: ['violet/History', 'pink/Free', 'red/English', 'yellow/Chem', 'orange/French'],
-        dayB: ['green/Free', 'blue/Theater Arts', 'tan/Math', 'violet/History', 'pink/Free'],
-        dayC: ['yellow/Chem', 'red/English', 'orange/French', 'green/Free', 'blue/Theater Arts'],
-        dayD: ['tan/Math', 'violet/History', 'pink/Free', 'red/English', 'yellow/Chem'],
-        dayE: ['orange/French', 'green/Free', 'blue/Theater Arts', 'tan/Math', 'violet/History'],
-        dayF: ['pink/Free', 'red/English', 'yellow/Chem', 'orange/French', 'green/Free'],
-        dayG: ['blue/Theater Arts', 'tan/Math', 'violet/History', 'pink/Free', 'red/English'],
-        dayH: ['yellow/Chem', 'orange/French', 'green/Free', 'blue/Theater Arts', 'tan/Math'],
-    },
-    GOLD: {
-        //Navy + Gold = same
-        //No M1 + M2 on schedule
 
         //Graham
         // dayA: ['violet/History', 'pink/Free', 'red/English', 'yellow/Chem', 'orange/French'],
@@ -29,16 +16,26 @@ const schedule = {
         // dayH: ['yellow/Chem', 'orange/French', 'green/Free', 'blue/Theater Arts', 'tan/Math'],
 
         //Me
-        //dayA: ['violet/History', 'pink/Math', 'red/CSA', 'yellow/Chem', 'orange/Spanish'],
-        // dayB: ['green/Free', 'blue/English', 'tan/Innovation', 'violet/History', 'pink/Math'],
-        // dayC: ['yellow/Chem', 'red/CSA', 'orange/Spanish', 'green/Free', 'blue/English'],
-        // dayD: ['tan/Innovation', 'violet/History', 'pink/Math', 'red/CSA', 'yellow/Chem'],
-        // dayE: ['orange/French', 'green/Free', 'blue/Theater Arts', 'tan/Math', 'violet/History'],
-        // dayF: ['pink/Math', 'red/CSA', 'yellow/Chem', 'orange/Spanish', 'green/Free'],
-        // dayG: ['blue/English', 'tan/Innovation', 'violet/History', 'pink/Math', 'red/CSA'],
-        // dayH: ['yellow/Chem', 'orange/Spanish', 'green/Free', 'blue/English', 'tan/Innovation'],
+        dayA: ['violet/History', 'pink/Math', 'red/CSA', 'yellow/Chem', 'orange/Spanish'],
+        dayB: ['green/Free', 'blue/English', 'tan/Innovation', 'violet/History', 'pink/Math'],
+        dayC: ['yellow/Chem', 'red/CSA', 'orange/Spanish', 'green/Free', 'blue/English'],
+        dayD: ['tan/Innovation', 'violet/History', 'pink/Math', 'red/CSA', 'yellow/Chem'],
+        dayE: ['orange/French', 'green/Free', 'blue/Theater Arts', 'tan/Math', 'violet/History'],
+        dayF: ['pink/Math', 'red/CSA', 'yellow/Chem', 'orange/Spanish', 'green/Free'],
+        dayG: ['blue/English', 'tan/Innovation', 'violet/History', 'pink/Math', 'red/CSA'],
+        dayH: ['yellow/Chem', 'orange/Spanish', 'green/Free', 'blue/English', 'tan/Innovation'],
 
         //Varun
+        //dayA: ['violet/History', 'pink/PreCalc', 'red/English', 'yellow/Chem', 'orange/French'],
+        // dayB: ['green/Band', 'blue/Free', 'tan/CSP', 'violet/History', 'pink/PreCalc'],
+        // dayC: ['yellow/Chem', 'red/English', 'orange/French', 'green/Band', 'blue/Free'],
+        // dayD: ['tan/CSP', 'violet/History', 'pink/PreCalc', 'red/English', 'yellow/Chem'],
+        // dayE: ['orange/French', 'green/Band', 'blue/Free', 'tan/CSP', 'violet/History'],
+        // dayF: ['pink/PreCalc', 'red/English', 'yellow/Chem', 'orange/French', 'green/Band'],
+        // dayG: ['blue/Free', 'tan/CSP', 'violet/History', 'pink/PreCalc', 'red/English'],
+        // dayH: ['yellow/Chem', 'orange/French', 'green/Band', 'blue/Free', 'tan/CSP'],
+
+        //Greg
         //dayA: ['violet/History', 'pink/Math', 'red/CSA', 'yellow/Chem', 'orange/Spanish'],
         // dayB: ['green/Free', 'blue/English', 'tan/Innovation', 'violet/History', 'pink/Math'],
         // dayC: ['yellow/Chem', 'red/CSA', 'orange/Spanish', 'green/Free', 'blue/English'],
